d4526735208a1d5ca7fd0751dcf464dc925802a7
SERGEY ZHAROV <zhss.83@yandex.ru>
2022-01-11 18:11:30 +0500

    Правки по четвёртому ревью.

--- backend/dockerhub.sh
+++ backend/dockerhub.sh
@@ -1,3 +1,5 @@
-docker login
-docker build -t zhss1983/foodgram:latest .
-docker push zhss1983/foodgram:latest
+#!/bin/bash
+echo 'docker build -t zhss1983/foodgram:latest ./'
+`docker build -t zhss1983/foodgram:latest ./ `
+echo 'docker push zhss1983/foodgram:latest'
+`docker push zhss1983/foodgram:latest `
